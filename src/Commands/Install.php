642c8e810794d02de302e75bef5e25831e7dc45a
Waygou <bruno.falcao@waygou.com>
2018-11-19 22:11:54 +0000

    Apply fixes from StyleCI

--- src/Commands/Install.php
+++ src/Commands/Install.php
@@ -4,9 +4,9 @@ namespace Waygou\XheetahInstaller\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
-use Symfony\Component\Process\Process;
 use Illuminate\Support\Facades\Storage;
 use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
 class Install extends Command
 {
